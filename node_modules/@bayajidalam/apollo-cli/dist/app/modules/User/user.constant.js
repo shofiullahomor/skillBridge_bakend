08781191e2e6958116499e8d437eaa8996861f3e
shofiullah <shofiullahomor1436@gmail.com>
2026-02-23 16:28:07 +0600

    skillBridge bakend basic setup done

---
+++ node_modules/@bayajidalam/apollo-cli/dist/app/modules/User/user.constant.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserSearchableFields = void 0;
+exports.UserSearchableFields = [
+// Add searchable fields here
+];
